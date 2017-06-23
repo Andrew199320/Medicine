3ca47907aefb8c08d915f31318f1e885dd34c457
Andrew199320 <gricukandrei199320@gmail.com>
2017-06-23 20:32:24 +0200

    Pagination

--- BookStore/WebUI/Controllers/BooksController.cs
+++ BookStore/WebUI/Controllers/BooksController.cs
@@ -4,20 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     public class BooksController : Controller
     {
         private IBookRepository repository;
+
+        // number of books on the page
+        public int pageSize = 2;
+
        public BooksController(IBookRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Books);
+            BooksListViewModel model = new BooksListViewModel
+            {
+                Books = repository.Books.OrderBy(book => book.BookId).Skip((page - 1) * pageSize).Take(pageSize),
+                PagingInfo = new PaginInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Books.Count()
+                }
+            };    
+            return View(model);
         }
     }
 }
---
+++ BookStore/WebUI/Models/PaginInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models
+{
+    public class PaginInfo
+    {
+        public int TotalItems { get; set; }     // Total amount of books
+        public int ItemsPerPage { get; set; }   // Books amount per page
+        public int CurrentPage { get; set; }    // Number of current page
+        public int TotalPages                   // Total pages amount
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
+        }
+    }
+}
